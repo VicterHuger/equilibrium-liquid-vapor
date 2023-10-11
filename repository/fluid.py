f2b1bdc89b1b3dad9603c6e68be2dbc0c76e4b0e
Victor Hugo Simões <victorhugobs@eq.ufrj.br>
2023-10-10 21:48:25 -0300

    feat: add fluids data 3-5 to repository create and add docs string

--- repository/fluid.py
+++ repository/fluid.py
@@ -1,22 +1,30 @@
+"""Repository file to fluid with create, delete and get functions
+"""
 import asyncio
 from database.tortoise.models import Fluid
 from database.excell.fluid_data import create_fluid_data
 
 
 async def create_fluids():
+    """A function to create fluid in datbase
+    """
     fluids_data = [create_fluid_data('Fluid1')]
     fluids_data.append(create_fluid_data('Fluid2'))
+    fluids_data.append(create_fluid_data('Fluid3'))
+    fluids_data.append(create_fluid_data('Fluid4'))
+    fluids_data.append(create_fluid_data('Fluid5'))
     fluids = await asyncio.gather(*[Fluid.create(**fluid) for fluid in fluids_data])
     for fluid in fluids:
         await fluid.save()
-        print(f"Fluid {fluid.id}:")
-        for field in Fluid._meta.fields_map:
-            print(f"  {field}: {getattr(fluid, field)}")
 
 
 async def delete_all_fluids():
+    """A function to delete all fluids in datbase
+    """
     await Fluid.filter().delete()
 
 
 async def get_all_fluids():
+    """A function to get all fluids in datbase
+    """
     return await Fluid.all()
