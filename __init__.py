b7f032647562e927d0eafda4a87dccf1c6597eeb
Victor Hugo Simões <victorhugobs@eq.ufrj.br>
2023-10-10 23:36:05 -0300

    feat: add repository call for component in init func

--- __init__.py
+++ __init__.py
@@ -1,9 +1,11 @@
 """file to inialize project"""
+import asyncio
 from dotenv import load_dotenv
 from tortoise import Tortoise, run_async
 from database.tortoise.tortoise_config import TORTOISE
-from database.tortoise.models import Fluid
+from database.tortoise.models import Component, Fluid
 from repository.fluid import get_all_fluids, create_fluids, delete_all_fluids
+from repository.component import create_components, delete_all_components, get_all_components
 
 load_dotenv()
 
@@ -23,13 +25,20 @@ async def init():
     """
     await create_tables()
     print('Tables Created')
-    await delete_all_fluids()
-    await create_fluids()
+    await asyncio.gather(delete_all_fluids(), delete_all_components())
+    await asyncio.gather(create_components(), create_fluids())
+
     fluids = await get_all_fluids()
     for fluid in fluids:
         print(fluid.name)
         for field in Fluid._meta.fields_map:
             print(f"  {field}: {getattr(fluid, field)}")
+
+    components = await get_all_components()
+    for component in components:
+        print(component.name)
+        for field in Component._meta.fields_map:
+            print(f"  {field}: {getattr(component, field)}")
 
     await Tortoise.close_connections()
 
