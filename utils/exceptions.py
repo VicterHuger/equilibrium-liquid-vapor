65476e07103a74114083b7eb1c7099effdb1b356
Victor Hugo Simões <victorhugobs@eq.ufrj.br>
2024-02-08 23:39:05 -0300

    feat: add a custom exception class for non convergence calculations

---
+++ utils/exceptions.py
@@ -0,0 +1,9 @@
+"""Exception classes"""
+
+
+class ConvergenceError(Exception):
+    """Exception Class for when a count operation reaches maximum
+
+    Args:
+        Exception (_type_): _description_
+    """
