80f18ad7ceb280d974bcf8468f22feb0fb1d6797
Victor Hugo Simões <victorhugobs@eq.ufrj.br>
2024-02-19 23:32:20 -0300

    refactor: remove unused def __str__

--- database/tortoise/models.py
+++ database/tortoise/models.py
@@ -25,9 +25,6 @@ class Component(Model):
     created_at = fields.DatetimeField(auto_now_add=True)
     updated_at = fields.DatetimeField(auto_now=True)
 
-    def __str__(self):
-        return self.name
-
 
 class Fluid(Model):
     """
